7d80693fc6b48013501d74c5dba292c4e21ae613
Exequiel <exe.pine@gmail.com>
2022-02-06 20:54:46 -0300

    agrego manejo de excepciones

---
+++ src/main/java/com/exepinero/couponwalletservice/exceptions/ApiExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.exepinero.couponwalletservice.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(value = {WalletNotFoundException.class, CouponNotFoundException.class})
+    public ResponseEntity<Object> handleNotFoundException(RuntimeException e){
+
+        // 1. Create payload containing exception details
+        HttpStatus notFound = HttpStatus.NOT_FOUND;
+
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                e.getCause(),
+                notFound,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        // 2. Return respons entity
+        return new ResponseEntity<>(apiException, notFound);
+    }
+}
